cc43fbf050f9f4514e874da9192b64974eb32639
Tomás Gadea <39812448+B4zy@users.noreply.github.com>
2021-05-20 21:44:32 -0300

    Delete gadea_tomas_grep1.sh

--- gadea_tomas_grep1.sh
+++
@@ -1,8 +0,0 @@
-grep -iE "x\B[0-9]{4}" grepdata.txt
-grep -E "^[0-9]{3}[[:space:]]" grepdata.txt
-grep -E "[[:space:]][[:digit:]]{1,2},[[:space:]]2[0-9]{3}" grepdata.txt
-grep -iE "[a][[:alpha:]][a]|[e][[:alpha:]][e]|[i][[:alpha:]][i]|[o]
-[[:alpha:]][o]|[u][[:alpha:]][u]" grepdata.txt 
-grep -v "^S" grepdata.txt
-grep -iE "[[:alnum:]]@[[:alpha:]].[[:alpha:]]" grepdata.txt
-
